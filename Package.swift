568263bce916cd32c0932e411018f90ea310aab2
Alexandr Polienko <>
2024-04-17 20:06:05 +0500

    - Example project removed - PrivacyInfo added

--- Package.swift
+++ Package.swift
@@ -42,6 +42,7 @@ let package = Package(
         .target(
             name: "AFNetworking",
             path: "AFNetworking",
+            resources: [.process("PrivacyInfo.xcprivacy")],
             publicHeadersPath: ""
         ),
         .target(
